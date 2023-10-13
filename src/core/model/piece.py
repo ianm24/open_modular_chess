1d0036da8a133c9a1c519f65843e1613dc34f874
ianm24 <ianmcdowell24@gmail.com>
2023-10-13 00:12:39 -0400

    Added board,player_control, and tests to Piece

---
+++ src/core/model/piece.py
@@ -0,0 +1,231 @@
+# Code by Chandler McDowell 2023
+from typing import Optional
+
+from core.exception.exception import MissingPieceCharException
+from core.exception.exception import PiecePxlHexOutOfBoundsException
+from core.exception.exception import NegativePiecePlayerNumException
+# TODO uncomment this once board updated with current_players
+# from core.exception.exception import NonCurrentPiecePlayerNumException
+from core.exception.exception import NegativePieceCoordinatesException
+
+from core.model.board import Board
+
+
+class Piece:
+    """Defines the base class for a piece."""
+    __slots__ = ('_piece_char', '_piece_pxl_hex', '_board',
+                 '_current_coords', '_player_controller')
+
+    def __init__(
+            self, piece_char: str, piece_pxl_hex: int, board: Board,
+            current_coords: Optional[tuple[int, int]] = None,
+            player_controller: Optional[int] = None
+    ):
+        """
+        Constructor for a piece.
+
+        Parameters:
+            :param piece_char: 1-character string that represent the piece in
+                the command line display
+            :param piece_pxl_hex: Hex value of 64-bit number representing an
+                8x8 pixel grid to display the piece in a GUI.
+            :param board: The board the piece belongs to.
+            :param current_coords: Represents the current placement of the
+                piece on the board. Default is (0,0)
+            :param player_controller: The player controlling this piece.
+                Default is 1.
+        """
+
+        # Ensure piece character is valid
+        if piece_char == "":
+            raise MissingPieceCharException(
+                "Error: Piece character for piece"
+                f" {self.__class__.__name__} is missing. Please set"
+                " piece character to a 1 character string.")
+
+        if len(piece_char) > 1:
+            print("Warning: Piece character for piece"
+                  f" {self.__class__.__name__} is too long. Length of piece"
+                  f" character is {len(piece_char)} which is longer than 1. "
+                  "Piece character has been set to the first character,"
+                  f"{piece_char[0]}.")
+            piece_char = piece_char[0]
+
+        self._piece_char = piece_char[0]
+
+        # Ensure piece pixel-hex is valid
+        if piece_pxl_hex < 1 or piece_pxl_hex > (2**64)-1:
+            raise PiecePxlHexOutOfBoundsException(
+                "Error: Piece pixel-hex for"
+                f" {self.__class__.__name__} is out of bounds. Pixel-hex"
+                f" is {piece_pxl_hex} which is not between 1 and 2^64 - 1.")
+
+        self._piece_pxl_hex = piece_pxl_hex
+
+        self._board = board
+
+        if current_coords is None:
+            current_coords = (0, 0)
+
+        if current_coords[0] < 0 or current_coords[1] < 0:
+            raise NegativePieceCoordinatesException(
+                "Error: Piece coordinates for"
+                f" {self.__class__.__name__} are negative. Coordinates"
+                f" are {current_coords} which should be greater or equal"
+                " to (0,0)")
+
+        self._current_coords = current_coords
+
+        if player_controller is None:
+            player_controller = 1
+
+        if player_controller < 0:
+            raise NegativePiecePlayerNumException(
+                "Error: Player controller for piece"
+                f" {self.__class__.__name__} is negative. Player controller"
+                f" is {player_controller} which is less than 0.")
+
+        # TODO uncomment this once board updated with current_players
+        # if player_controller not in self._board.current_players:
+        #     raise NonCurrentPiecePlayerNumException(
+        #         "Error: Player controller for piece"
+        #         f" {self.__class__.__name__} is non-current. Player"
+        #         f" controller is {player_controller} which is not a current"
+        #         f" player: {self._board.current_players}.")
+
+        self._player_controller = player_controller
+
+    @property
+    def piece_char(self) -> str:
+        '''
+        Gets the character representation for the piece.
+
+        :return piece_char: 1-character string that represent the piece in
+            the command line display
+        '''
+        return self._piece_char
+
+    @property
+    def piece_pxl_hex(self) -> int:
+        '''
+        Gets the pixel-hex representation for the piece.
+
+        :return piece_pxl_hex: Hex value of 64-bit number representing an
+            8x8 pixel grid to display the piece in a GUI.
+        '''
+
+        return self._piece_pxl_hex
+
+    @property
+    def current_players(self) -> list[int,]:
+        '''
+        Gets the list of current players from board.
+        '''
+
+        # return self._board.current_players
+        return [0]  # TODO replace with commented line once board updated
+
+    @property
+    def current_coords(self) -> tuple[int, int]:
+        '''
+        Gets the current coordinates of the piece
+        '''
+        return self._current_coords
+
+    @property
+    def player_controller(self) -> int:
+        '''
+        Gets the current player controller.
+
+        :return player_controller: Number associated with player currently"
+        " controlling the piece.
+        '''
+        return self._player_controller
+
+    def list_moves(self) -> list[Optional[tuple[int, int]],]:
+        '''
+        Gives a list of all available moves for the piece.
+
+        :return moves_list: List of moves currently able to be performed by
+            the piece.
+        '''
+
+        return [(0, 1)]
+
+    def move(self, coords: tuple[int, int]) -> bool:
+        '''
+        Performs an action on the piece using arguments
+
+        :return success: True on successful move, False otherwise.
+        '''
+
+        # Check if move is valid
+        if coords not in self.list_moves():
+            print("Invalid Move.")
+            return False
+
+        self._current_coords = coords
+        return True
+
+    def set_player_control(self, new_player: int) -> bool:
+        '''
+        Changes the player in control of this piece.
+
+        :param new_player: Player number taking control of the piece.
+        :return success: True on successful action, False otherwise.
+        '''
+
+        if new_player < 0:
+            print("Invalid Player Number (Negative).")
+            return False
+
+        valid_players = self.current_players
+
+        if new_player not in valid_players:
+            print(f"Invalid Player Number. (Valid Numbers: {valid_players})")
+            return False
+
+        self._player_controller = new_player
+        return True
+
+    def list_actions(self) -> list[str,]:
+        '''
+        Gives a list of all available actions for the piece.
+
+        :return action_list: List of actions currently able to be performed by
+            the piece.
+        '''
+
+        return ["move", "control"]
+
+    def perform_action(self, action: str, args: list[str,]) -> bool:
+        '''
+        Performs an action on the piece using arguments
+
+        :return success: True on successful action, False otherwise.
+        '''
+
+        # Check if action is valid
+        if action not in self.list_actions():
+            print(f"Invalid Action. Valid Actions: {self.list_actions()}")
+            return False
+
+        if action == "move":
+            try:
+                x_coord = int(args[0])
+                y_coord = int(args[1])
+            except ValueError:
+                print(f"Invalid arguments for action {action}. Arguments must"
+                      " be two integers.")
+                return False
+
+            return self.move((x_coord, y_coord))
+        elif action == "control":
+            try:
+                player_controller = int(args[0])
+            except ValueError:
+                print(f"Invalid arguments for action {action}. Argument must"
+                      " be one integer.")
+                return False
+
+            return self.set_player_control(player_controller)
---
+++ tests/unit/test_piece.py
@@ -0,0 +1,191 @@
+import pytest
+
+from core.model.piece import Piece
+from core.model.board import Board
+
+from core.exception.exception import MissingPieceCharException
+from core.exception.exception import PiecePxlHexOutOfBoundsException
+from core.exception.exception import NegativePieceCoordinatesException
+from core.exception.exception import NegativePiecePlayerNumException
+# TODO uncomment this once board updated with current_players
+# from core.exception.exception import NonCurrentPiecePlayerNumException
+
+'''___init___ tests'''
+
+
+def test_missing_piece_char():
+    """Ensures proper function when a piece has an empty piece character"""
+    with pytest.raises(MissingPieceCharException):
+        Piece("", 1, Board.empty(), (1, 1), 1)
+
+
+def test_long_piece_char():
+    """Ensures proper function when a piece has a piece character length > 1"""
+    p = Piece("Test", 1, Board.empty())
+    assert p.piece_char == 'T'
+
+
+def test_piece_char_immutability():
+    """Ensures proper function when a piece has a piece character length > 1"""
+    p = Piece('t', 1, Board.empty())
+    with pytest.raises(AttributeError):
+        p.piece_char = 'q'
+
+
+def test_piece_char():
+    """Ensures proper function when a piece has a valid piece character"""
+    p = Piece('t', 1, Board.empty())
+    assert p.piece_char == 't'
+
+
+def test_negative_piece_pxl_hex():
+    """Ensures proper function when a piece has a negative pixel-hex"""
+    with pytest.raises(PiecePxlHexOutOfBoundsException):
+        Piece('t', -1, Board.empty())
+
+
+def test_large_piece_pxl_hex():
+    """Ensures proper function when a piece has a pixel-hex > 64-bits"""
+    with pytest.raises(PiecePxlHexOutOfBoundsException):
+        Piece('t', 2**64, Board.empty())
+
+
+def test_piece_pxl_hex_immutability():
+    """Ensures proper function when a piece has a piece character length > 1"""
+    p = Piece('t', 1, Board.empty())
+    with pytest.raises(AttributeError):
+        p.piece_pxl_hex = 2
+
+
+def test_piece_pxl_hex():
+    """Ensures proper function when a piece has a valid piece pixel-hex"""
+    p = Piece('t', 1, Board.empty())
+    assert p.piece_pxl_hex == 1
+
+
+def test_empty_current_coords():
+    """Ensures proper function when a piece has empty current_coords"""
+    p = Piece("Test", 1, Board.empty())
+    assert p.current_coords == (0, 0)
+
+
+def test_negative_x_coord():
+    """Ensures proper function when a piece has a negative x coordinate"""
+    with pytest.raises(NegativePieceCoordinatesException):
+        Piece('t', 1, Board.empty(), (-1, 0))
+
+
+def test_negative_y_coord():
+    """Ensures proper function when a piece has a negative y coordinate"""
+    with pytest.raises(NegativePieceCoordinatesException):
+        Piece('t', 1, Board.empty(), (0, -1))
+
+
+def test_current_coords_immutability():
+    """Ensures proper function when a piece has a piece character length > 1"""
+    p = Piece('t', 1, Board.empty())
+    with pytest.raises(AttributeError):
+        p.current_coords = (1, 1)
+
+
+def test_current_coords():
+    """Ensures proper function when a piece has valid current coordinates"""
+    p = Piece('t', 1, Board.empty(), (1, 1))
+    assert p.current_coords == (1, 1)
+
+
+def test_negative_player_controller():
+    """Ensures proper function when a piece has a negative player controller"""
+    with pytest.raises(NegativePiecePlayerNumException):
+        Piece('t', 1, Board.empty(), player_controller=-1)
+
+
+# TODO uncomment this once board updated with current_players
+# def test_non_current_player_controller():
+#     """Ensures proper function when a piece has a non-current
+#     player controller"""
+#     with pytest.raises(NonCurrentPiecePlayerNumException):
+#         Piece('t', 1, Board(1, 1, [""],
+#                             current_players=[0, 1, 2]), player_controller=3)
+
+
+def test_player_controller():
+    """Ensures proper function when a piece has valid player controller"""
+#         Piece('t', 1, Board(1, 1, [""],
+#                             current_players=[0, 1, 2]), player_controller=3)
+    p = Piece('t', 1, Board.empty(), player_controller=2)
+    # TODO replace with commented line once board updated
+    assert p.player_controller == 2
+
+
+'''Move Tests'''
+
+
+def test_invalid_move():
+    '''Ensures proper function of move with invalid input'''
+    p = Piece('t', 1, Board.empty())
+    assert p.move((2, 2)) is False and p.current_coords == (0, 0)
+
+
+def test_move():
+    '''Ensures proper function of move with valid input'''
+    p = Piece('t', 1, Board.empty())
+    assert p.move((0, 1)) is True and p.current_coords == (0, 1)
+
+
+'''SetPlayerControl Tests'''
+
+
+def test_set_negative_player_control():
+    '''Ensures proper function of set_player_control with negative input'''
+    p = Piece('t', 1, Board.empty())
+    assert p.set_player_control(-1) is False and p.player_controller == 1
+
+
+def test_set_noncurrent_player_control():
+    '''Ensures proper function of set_player_control with noncurrent player'''
+    p = Piece('t', 1, Board.empty())
+    assert p.set_player_control(3) is False and p.player_controller == 1
+
+
+def test_set_player_control():
+    '''Ensures proper function of set_player_control with valid input'''
+    p = Piece('t', 1, Board.empty())
+    assert p.set_player_control(0) is True and p.player_controller == 0
+
+
+'''PerformAction Tests'''
+
+
+def test_perform_invalid_action():
+    '''Ensures proper function of perform_action with valid input'''
+    p = Piece('t', 1, Board.empty())
+    assert p.perform_action("test", []) is False
+
+
+def test_perform_invalid_move_args_action():
+    '''Ensures proper function of perform_action with invalid move args'''
+    p = Piece('t', 1, Board.empty())
+    assert p.perform_action("move", ["t", "1"]) is False and p.perform_action(
+        "move", ["1", "t"]) is False and p.current_coords == (0, 0)
+
+
+def test_perform_valid_move_action():
+    '''Ensures proper function of perform_action with valid move args'''
+    p = Piece('t', 1, Board.empty())
+    assert p.perform_action(
+        "move", ["0", "1"]) is True and p.current_coords == (0, 1)
+
+
+def test_perform_invalid_control_arg_action():
+    '''Ensures proper function of perform_action with invalid control arg'''
+    p = Piece('t', 1, Board.empty())
+    assert p.perform_action(
+        "control", ["t"]) is False and p.player_controller == 1
+
+
+def test_perform_valid_control_arg_action():
+    '''Ensures proper function of perform_action with valid move arg'''
+    p = Piece('t', 1, Board.empty())
+    assert p.perform_action(
+        "control", ["0"]) is True and p.player_controller == 0
