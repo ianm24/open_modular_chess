606d4f984cfb18f3a9c79379d5fd135f086a0cec
ianm24 <ianmcdowell24@gmail.com>
2023-10-19 19:06:36 -0400

    Combined tests based on file

--- tests/unit/test_board.py
+++ tests/unit/test_board.py
@@ -12,6 +12,10 @@ from omc.core.model.board import Board
 from omc.core.model.board import Piece
 from omc.core.model.board import Player
 
+'''Board Class Tests'''
+
+'''Piece Class Tests'''
+
 
 class SimplePiece(Piece):
     """
@@ -507,3 +511,53 @@ def test_perform_valid_control_arg_action():
 
     assert p.perform_action("control", ["0"]) is True
     assert p.player_controller == expected_player_controller
+
+
+'''Player Class Tests'''
+
+'''__init__ tests'''
+
+
+def test_player_number_immutable():
+    """
+    Ensures proper function when a player has an attempted player_number edit
+    """
+    expected_player_number = 1
+
+    p = Player(1)
+
+    with pytest.raises(AttributeError):
+        p.player_number = 2
+    assert p.player_number == expected_player_number
+
+
+'''AddPiece Tests'''
+
+
+@pytest.mark.xfail(reason="Add piece does not yet check player controller")
+def test_add_piece_not_controlled():
+    """
+    Ensures proper functionality when a player adds a piece it doesn't control
+    """
+    test_board = Board.empty(np.array([8, 8]))
+    player2 = Player(2)
+    test_board.add_player(Player(1))
+    test_board.add_player(player2)
+    piece = SimplePiece(test_board)
+
+    with pytest.raises(PlayerNotControllingPieceException):
+        player2.add_piece(piece)
+    assert piece not in player2.pieces
+
+
+@pytest.mark.xfail(reason="Add piece does not return a success boolean")
+def test_add_piece():
+    """
+    Ensures proper functionality when a player adds a valid piece
+    """
+    test_board = Board.empty(np.array([8, 8]))
+    player = Player(1)
+    test_board.add_player(player)
+    piece = SimplePiece(test_board)
+
+    assert player.add_piece(piece) is True
